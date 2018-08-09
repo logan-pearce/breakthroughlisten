86eddbbb9f5616c485bede02fe7012116200f2ee
Logan Pearce <loganpearce55@gmail.com>
2018-08-08 23:04:49 -0700

    Update get_targets.py

--- get_targets/get_targets.py
+++ get_targets/get_targets.py
@@ -204,7 +204,7 @@ db = MySQLdb.connect(host='104.154.94.28',db='loganp',\
 # Using mysql connector/python:
 # import mysql.connector
 # db = mysql.connector.connect(option_files='/Users/loganpearce/.my.cnf',host='104.154.94.28',\
-#                              database='loganp')
+#                              database='loganp',autocommit=True,allow_local_infile=True)
 
 ############### Get all targets in the database within beam pointings:
 print 'Retrieving targets...'
